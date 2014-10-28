2c63701300c0a83b65ef3500861d60df9a7f665f
Walter C. Pelissero <walter@pelissero.de>
2014-10-28 15:43:16 +0100

    Added togglePin() and refactored the rest of the code accordingly.

--- sketch.cc
+++ sketch.cc
@@ -12,21 +12,36 @@
 
 // how long after a push the relay should be activated.
 #define ACTIVE_LENGTH 10
+#define BLINK_SPEED 300
 
 int relayPin = 8;
 int buttonPin = 12;		// digital input
 int ledPin = 13;
 
-void
+static void
+togglePin (int pin)
+{
+  digitalWrite(pin, !digitalRead(pin));
+}
+
+static void
 blinkLED (int times, int ms)
 {
+  times *= 2;
+  ms /= 2;
+  digitalWrite(ledPin, LOW);
   while (times--)
     {
-      digitalWrite(ledPin, HIGH);
-      delay(ms);
-      digitalWrite(ledPin, LOW);
+      togglePin(ledPin);
       delay(ms);
     }
+  digitalWrite(ledPin, LOW);
+}
+
+static int
+isButtonPressed ()
+{
+  return digitalRead(buttonPin) == LOW;
 }
 
 void
@@ -38,10 +53,10 @@ setup ()
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);
-  blinkLED(7, 200);
+  blinkLED(ACTIVE_LENGTH, BLINK_SPEED / 2);
 }
 
-int activated = 0;
+static int relayOn = 0;
 
 void
 loop ()
@@ -54,19 +69,19 @@ loop ()
   if (command >= '0' && command <= '5')
     Serial.println(analogRead(command - '0'));
   if (command == 'S')
-    Serial.println(activated);
-  if (digitalRead(buttonPin) == LOW || command == 'T')
+    Serial.println(relayOn);
+  if (isButtonPressed() || command == 'T')
     {
-      activated = ACTIVE_LENGTH * 1000;
+      relayOn = ACTIVE_LENGTH * 1000;
       digitalWrite(relayPin, HIGH); 
       digitalWrite(ledPin, HIGH);
     }
-  if (activated > 0)
+  if (relayOn)
     {
-      activated--;
-      if (activated % 300 == 0)
-	digitalWrite(ledPin, !digitalRead(ledPin)); // blink the LED
-      if (!activated)
+      relayOn--;
+      if (relayOn % BLINK_SPEED == 0)
+	togglePin(ledPin);	// blink the LED
+      if (!relayOn)
 	{
 	  digitalWrite(ledPin, LOW);
 	  digitalWrite(relayPin, LOW);
